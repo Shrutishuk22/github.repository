cb92e00b9e2dd68d4fc35b0839cb0d4abb4fff5b
shrutishuk22 <shrutishuk220503@gmail.com>
2024-10-18 01:01:13 +0530

    first commit

---
+++ assets/app.js
@@ -0,0 +1,82 @@
+console.log("shruti")
+let name = "shruti";
+let agee = 20;
+console.log(name);
+console.log(agee);
+let a=10;
+let b=100;
+console.log("sum is : ",a+b)
+console.log(a+b);
+
+
+// traffic light system;
+let color = "red";
+if(color === "red"){
+    console.log("stop");
+}
+if (color === "yellow"){
+    console.log("wait");
+}
+if (color === "green"){
+    console.log("go");
+}
+
+// vote 
+let age = 18;
+if(age>18){
+    console.log("you can vote");
+}
+else if(age=18){
+    console.log("you can vote");
+}
+else{
+    console.log("you can't vote");
+}
+
+
+// grading system...
+let marks = 80;
+if(marks >=90){
+    console.log("grade A");
+}
+else if( marks<=80){
+    console.log("grade B");
+}
+else if(marks<=70){
+    console.log("grade C");
+    }
+else{
+    console.log("fail");
+}
+
+
+// popcorn price...
+let size = "xxl"
+if(size === "s"){
+    console.log("price is 50");
+}
+else if(size === "m"){
+    console.log("price is 70");
+}
+else if(size === "xl"){
+    console.log("price is 150");
+}
+else{
+    console.log("price is 250");
+}
+
+
+// nested if else
+let markss = 33;
+if(marks>=33){
+    console.log("pass");
+ if(marks>80){
+    console.log("o")
+ }
+ else{
+    console.log("pass with distinction");
+ }
+}
+else{
+    console.log("fail");
+}
